ce7f234cf6f04f62a10ee69206e3cf27652efd3b
fmasouza <fabmacsouza@gmail.com>
2020-10-05 18:14:37 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Betsey Rosebotham', '012345678901', 5500.0, TIMESTAMP WITH TIME ZONE '1984-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucio Goodall', '45612378932', 4200.0, TIMESTAMP WITH TIME ZONE '1986-05-01T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lynsey Doick', '78945612320', 6250.0, TIMESTAMP WITH TIME ZONE '1980-12-30T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Garrek Millsom', '85974841526', 3600.0, TIMESTAMP WITH TIME ZONE '1979-07-14T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Alice Foro', '32165498741', 4000.0, TIMESTAMP WITH TIME ZONE '1984-03-03T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bernardine Hamsher', '36925814720', 3650.0, TIMESTAMP WITH TIME ZONE '1986-08-16T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Renard Howship', '96385274125', 2560.0, TIMESTAMP WITH TIME ZONE '1980-07-05T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Paxon MacDunleavy', '65498732165', 5890.0, TIMESTAMP WITH TIME ZONE '1985-11-24T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gilli Kun', '25826539845', 6592.0, TIMESTAMP WITH TIME ZONE '1983-11-11T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Kleon Jacklin', '69832514585', 4589.0, TIMESTAMP WITH TIME ZONE '1986-06-20T20:50:07.12345Z', 1);
